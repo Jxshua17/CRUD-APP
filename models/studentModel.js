f27f5ca8e0e743090b0ae8226adc156033f2cb0f
Joshua <112779066+Jxshua17@users.noreply.github.com>
2026-05-10 18:29:56 +0100

    created studentSchema

---
+++ models/studentModel.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const studentSchema = new mongoose.Schema({
+  id: { type: Number, unique: true },
+  name: { type: String, required: true },
+  age: { type: Number, required: true },
+  form: { type: Number, required: true },
+});
+
+module.exports = mongoose.Model("Student", studentSchema);
